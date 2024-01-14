ea689c005af87ad4584f0a3a06ca05683d2c60b2
VSempiternalis <vsempiternalis@gmail.com>
2024-01-15 03:55:29 +0800

    Poopoo

--- Assets/@Code/MainMenu/MainMenuManager.cs
+++ Assets/@Code/MainMenu/MainMenuManager.cs
@@ -1,7 +1,13 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour {
+    [SerializeField] private uiAnimGroup loadingScreen;
+    [SerializeField] private RectTransform loadingProgress;
+    [SerializeField] private float loadTransitionTime = 2f;
+
     private void Start() {
         
     }
@@ -10,8 +16,25 @@ public class MainMenuManager : MonoBehaviour {
         
     }
 
-    public void StartGameOfflineFreeride() {
-        SceneManager.LoadScene("SCENE - Game");
+    public void StartGame(string sceneName) {
+        StartCoroutine(LoadLevel(sceneName));
+    }
+
+    IEnumerator LoadLevel(string sceneName) {
+        loadingScreen.In();
+        loadingScreen.GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        // yield return new WaitForSeconds(loadTransitionTime);
+
+        // SceneManager.LoadScene("SCENE - Game");
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        while(!operation.isDone) {
+            float progress = Mathf.Clamp01(operation.progress/0.9f);
+            print(progress);
+            loadingProgress.LeanScaleX(progress, 1f);
+            yield return null;
+        }
     }
 
     public void ExitGame() {
